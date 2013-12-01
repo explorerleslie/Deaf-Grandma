93de900a9888d36d78d03ea1f973e951316fc431
Leslie Gerhat <explorerleslie@gmail.com>
2013-11-30 23:18:22 -0500

    Add randomness to the year Grandma shouts.

--- deaf-grandma.rb
+++ deaf-grandma.rb
@@ -1,14 +1,16 @@
 def deaf_grandma
-    while true
-        command = gets.chomp
-        if command == 'I love ya, Grandma, but I\'ve got to go.'
-            break
-        elsif command == command.upcase
-            puts 'NO, NOT SINCE 1983!'
-        else
-            puts 'HUH?! SPEAK UP, SONNY!'
-        end
+  while true
+    command = gets.chomp
+    if command == 'I love ya, Grandma, but I\'ve got to go.'
+      break
+    elsif command == command.upcase
+      bye_counter = 0
+      puts "NO, NOT SINCE #{rand(1930..1950).to_s}!"
+    else
+      bye_counter = 0
+      puts 'HUH?! SPEAK UP, SONNY!'
     end
+  end
 end
 
 deaf_grandma
