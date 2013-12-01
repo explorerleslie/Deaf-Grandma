53e33211097f78cc9f4f5f98b33369b223a38f76
Leslie Gerhat <explorerleslie@gmail.com>
2013-12-01 20:47:47 +0000

    Refactor 2 new lines exit criteria.

--- deaf-grandma.rb
+++ deaf-grandma.rb
@@ -1,16 +1,16 @@
 def deaf_grandma
-  while true
+  silent_counter = 0
+  while silent_counter < 2
     command = gets.chomp
     if command == ''
-      command = gets.chomp
-      if command == ''
-        break
-      end
+      silent_counter += 1
     elsif command == "I love ya, Grandma, but I\'ve got to go."
       break
     elsif command == command.upcase
+      silent_counter = 0
       puts "NO, NOT SINCE #{rand(1930..1950).to_s}!"
     else
+      silent_counter = 0
       puts "HUH?! SPEAK UP, SONNY!"
     end
   end
