90a54a18b4d01d9edbedaab44da7331a2c035ae0
Leslie Gerhat <explorerleslie@gmail.com>
2013-12-01 21:22:51 +0000

    DRY up AM/PM response logic to later implement in other cases.

--- deaf-grandma.rb
+++ deaf-grandma.rb
@@ -1,15 +1,21 @@
+def am_pm_response(am_response, pm_response)
+  now = Time.now.strftime("%p")
+  if now == 'AM'
+    puts am_response
+  else
+    puts pm_response
+  end
+end
+
 def deaf_grandma
   silent_counter = 0
   now = Time.now.strftime("%p")
   puts 'Grandma pinched your cheeks!'
   while silent_counter < 2
     command = gets.chomp
+    #TODO change if statements to case statements?
     if command[0,1] == '<' #indicates a non-verbal action
-      if now == 'AM'
-        puts 'Come here and give Grandma a kiss!'
-      else
-         puts 'GO AWAY.'
-      end
+      am_pm_response('Come here and give Grandma a kiss!', 'GO AWAY.')
     elsif command == ''
       silent_counter += 1
     elsif command == "I love ya, Grandma, but I\'ve got to go."
