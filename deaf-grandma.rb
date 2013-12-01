92fe33019cc9a0b6ab5e89b1f240e9544cb2edf3
Leslie Gerhat <explorerleslie@gmail.com>
2013-11-30 23:26:12 -0500

    Removed unused counter.

--- deaf-grandma.rb
+++ deaf-grandma.rb
@@ -9,10 +9,8 @@ def deaf_grandma
     elsif command == 'I love ya, Grandma, but I\'ve got to go.'
       break
     elsif command == command.upcase
-      bye_counter = 0
       puts "NO, NOT SINCE #{rand(1930..1950).to_s}!"
     else
-      bye_counter = 0
       puts 'HUH?! SPEAK UP, SONNY!'
     end
   end
