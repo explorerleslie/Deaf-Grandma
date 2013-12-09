32fa5d2b07ee26b9cd0db50b1a03cbe4e361d2ac
Leslie Gerhat <explorerleslie@gmail.com>
2013-12-09 02:17:28 +0000

    Refactor am_pm_response to be a shorter method.

--- deaf-grandma.rb
+++ deaf-grandma.rb
@@ -1,20 +1,17 @@
 def am_pm_response(am_response, pm_response)
   now = Time.now.strftime("%p")
-  if now == 'AM'
-    puts am_response
-  else
-    puts pm_response
-  end
+  puts now == 'AM' ?  am_response : pm_response
 end
 
+# Methods should be less than 5 lines long! Sandy Metz https://www.youtube.com/watch?v=npOGOmkxuio
+# Classes should not be longer than 100 lines.
 def deaf_grandma
   silent_counter = 0
-  now = Time.now.strftime("%p")
   puts 'Grandma pinched your cheeks!'
   while silent_counter < 2
     command = gets.chomp
     #TODO change if statements to case statements?
-    if command[0,1] == '<' #indicates a non-verbal action
+    if command[0] == '<' #indicates a non-verbal action
       am_pm_response('Come here and give Grandma a kiss!', 'GO AWAY.')
     elsif command == ''
       silent_counter += 1
@@ -22,7 +19,7 @@ def deaf_grandma
       break
     elsif command == command.upcase
       silent_counter = 0
-      puts "NO, NOT SINCE #{rand(1930..1950).to_s}!"
+      puts "NO, NOT SINCE #{rand(1930..1950)}!"
     else
       silent_counter = 0
       puts "HUH?! SPEAK UP, SONNY!"
